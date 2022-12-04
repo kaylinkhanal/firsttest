d7c32560d55954f64298526107a9ba71d93f0b75
kaylinkhanal10 <kaylin.k@foundationai.com>
2022-12-04 18:57:33 +0545

    added a new const

--- dinasaur copy.js
+++ dinasaur copy.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-i am copy
-
-const a = 20;
-const z = 200
->>>>>>> e815d3375aa76837f3c371d716b08242e9c50c7b
 
 
 
@@ -17,12 +10,8 @@ a="hsgsgsgsggsgs"
 function kCHa(){
     console.log("Thik cha");
 }
-<<<<<<< HEAD
-kCHa()
-=======
 
 
 function sandip(){
     return 'sandip'
 }
->>>>>>> e815d3375aa76837f3c371d716b08242e9c50c7b
